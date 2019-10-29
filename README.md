c69762a21caf8e9be1cc7007f532a748b7ed55d7
Joey <rogues.jonathan.168@gmail.com>
2019-10-29 20:33:41 +0100

    Add helper.sh to help developers

--- README.md
+++ README.md
@@ -9,3 +9,10 @@ npm install
 ```bash
 docker run -d -p 8000:8000 amazon/dynamodb-local
 ```
+
+# How to know what's in dynamo
+
+```bash
+sudo apt install jq awscli
+./helper.sh
+```
---
+++ helper.sh
@@ -0,0 +1,23 @@
+export AWS_DEFAULT_REGION=localhost
+export DYNAMO_URL=http://localhost:8000
+
+tables=`aws dynamodb list-tables --endpoint $DYNAMO_URL | jq .TableNames[] | sed 's/"//g' | xargs`
+
+for table in $tables
+do
+  fields=`aws dynamodb scan --table-name $table --endpoint $DYNAMO_URL | jq '.Items[] | "\(keys[])"' | sed 's/"//g' | sort -u | xargs`
+
+  echo "+---------------------------+"
+  printf "%s%-25.25s%s" "| " $table " |"
+  echo
+  echo "+---------------------------+"
+
+  for field in $fields
+  do
+    printf "%s%-25.25s%s" "| " $field " |"
+    echo
+  done
+
+  echo "+---------------------------+"
+  echo
+done
